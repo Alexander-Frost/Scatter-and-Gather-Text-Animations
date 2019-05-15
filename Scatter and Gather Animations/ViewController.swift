ec5a3b70e2d18fc6ab7b21fd0fbdda1295b39136
Alex <alexcc17@gmail.com>
2019-05-15 18:26:51 -0400

    Initial Commit

---
+++ Scatter and Gather Animations/ViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  Scatter and Gather Animations
+//
+//  Created by Alex on 5/15/19.
+//  Copyright © 2019 Alex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: - Constants
+    
+    var shouldScramble: Bool = true
+    var lblArray = [UILabel]()
+    var colorArray: [UIColor] = [.red, .green, .blue, .yellow, .purple, .orange]
+
+    // MARK: - Outlets
+    
+    @IBOutlet var lLbl: UILabel!
+    @IBOutlet var aLbl: UILabel!
+    @IBOutlet var mLbl: UILabel!
+    @IBOutlet var bLbl: UILabel!
+    @IBOutlet var dLbl: UILabel!
+    @IBOutlet var finalALbl: UILabel!
+    
+    // MARK: - Actions
+    
+    @IBAction func toggle(_ sender: UIBarButtonItem) {
+        if shouldScramble {
+            UIView.animate(withDuration: 2.0, animations: {
+                for lbl in self.lblArray {
+                    lbl.transform = CGAffineTransform(rotationAngle: CGFloat.pi/CGFloat(self.random()))
+                    lbl.center = CGPoint(x: self.randomX(), y: self.randomY())
+                    lbl.layer.backgroundColor = self.colorArray.randomItem()?.cgColor
+                    lbl.textColor = self.colorArray.randomItem()
+                }
+            })
+        } else {
+            UIView.animate(withDuration: 3.0, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 0, options: [], animations: {
+                for lbl in self.lblArray {
+                    lbl.transform = .identity
+                    lbl.layer.backgroundColor = UIColor.white.cgColor
+                    lbl.textColor = .black
+                }
+            }, completion: nil)
+        }
+        shouldScramble.toggle()
+    }
+    
+    //MARK: - VC Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        lblArray += [lLbl, aLbl, mLbl, bLbl, dLbl, finalALbl]
+    }
+    
+    //MARK: - Functions
+    
+    func random() -> Int{
+        return Int.random(in: -10...10)
+    }
+    func randomX() -> CGFloat{
+        return CGFloat(Int.random(in: 0...Int(self.view.bounds.size.width)))
+    }
+    func randomY() -> CGFloat{
+        return CGFloat(Int.random(in: 0...Int(self.view.bounds.size.height)))
+    }
+
+}
