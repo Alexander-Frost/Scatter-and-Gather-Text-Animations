668238c22231e0421856b9cdb9bb4f450956aa9b
Alex <alexcc17@gmail.com>
2019-05-16 18:40:38 -0400

    Add to animations

--- Scatter and Gather Animations/ViewController.swift
+++ Scatter and Gather Animations/ViewController.swift
@@ -32,15 +32,18 @@ class ViewController: UIViewController {
             UIView.animate(withDuration: 2.0, animations: {
                 for lbl in self.lblArray {
                     lbl.transform = CGAffineTransform(rotationAngle: CGFloat.pi/CGFloat(self.random()))
-                    lbl.center = CGPoint(x: self.randomX(), y: self.randomY())
+                    lbl.transform = CGAffineTransform(translationX: self.randomX(), y: self.randomY())
+//                    lbl.center = CGPoint(x: self.randomX(), y: self.randomY())
                     lbl.layer.backgroundColor = self.colorArray.randomItem()?.cgColor
                     lbl.textColor = self.colorArray.randomItem()
+//                    lbl.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
                 }
             })
         } else {
-            UIView.animate(withDuration: 3.0, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 0, options: [], animations: {
+            UIView.animate(withDuration: 3.0, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 0, options: .repeat, animations: {
                 for lbl in self.lblArray {
                     lbl.transform = .identity
+//                    lbl.transform = CGAffineTransform(scaleX: 0.0, y: 0.0)
                     lbl.layer.backgroundColor = UIColor.white.cgColor
                     lbl.textColor = .black
                 }
@@ -54,6 +57,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         lblArray += [lLbl, aLbl, mLbl, bLbl, dLbl, finalALbl]
+        for lbl in lblArray {
+            lbl.transform = CGAffineTransform(scaleX: 0.0, y: 0.0)
+        }
     }
     
     //MARK: - Functions
